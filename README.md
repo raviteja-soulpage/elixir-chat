bab98b334d8addbf106322eb80f927c0c1171b7f
venkatesh <venkatesh.marreboyina@soulpageit.com>
2020-06-04 13:38:21 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Run `$ mix deps.get`
+#Run `$iex -S mix`
+#Navigate to `http://localhost:4001/` in two different browsers.
---
+++ lib/my_websocket_app.ex
@@ -0,0 +1,50 @@
+defmodule MyWebsocketApp do
+  @moduledoc """
+  Documentation for `MyWebsocketApp`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MyWebsocketApp.hello()
+      :world
+
+  """
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      Plug.Cowboy.child_spec(
+        scheme: :http,
+        plug: MyWebsocketApp.Router,
+        options: [
+          dispatch: dispatch(),
+          port: 4000
+        ]
+      ),
+      Registry.child_spec(
+        keys: :duplicate,
+        name: Registry.MyWebsocketApp
+      )
+    ]
+
+    opts = [strategy: :one_for_one, name: MyWebsocketApp.Application]
+    Supervisor.start_link(children, opts)
+  end
+
+  defp dispatch do
+    [
+      {:_,
+        [
+          {"/ws/[...]", MyWebsocketApp.SocketHandler, []},
+          {:_, Plug.Cowboy.Handler, {MyWebsocketApp.Router, []}}
+        ]
+      }
+    ]
+  end
+  def hello do
+    :world
+  end
+end
